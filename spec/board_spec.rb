dedc6cba212ce70599fb080996e65e046c0cce7a
Brittany Blake <brittany.blake49@gmail.com>
2020-05-12 10:43:14 +0200

    Fix linter errors

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,51 +1,50 @@
 require_relative '../lib/board.rb'
 
 RSpec.describe Board do
-    let(:board) {Board.new}
-    let(:grid) {board.instance_variable_get(:@grid)}
+  let(:board) { Board.new }
+  let(:grid) { board.instance_variable_get(:@grid) }
 
-    describe '#initialize' do
-      it 'should set the grid to a 2D array with 3 rows and 3 columns' do
-        expect(grid.length).to eq(3)
-        expect(grid[0].length).to eq(3)
-      end
-
-      it 'should not set the grid as an empty array' do
-        expect(grid).not_to eq([])
-      end
+  describe '#initialize' do
+    it 'should set the grid to a 2D array with 3 rows and 3 columns' do
+      expect(grid.length).to eq(3)
+      expect(grid[0].length).to eq(3)
     end
 
-    describe '#[]' do
-      it 'should accept a pair of indices to represent the chosen position on the board' do
-         pos = [0, 0]
-        expect(board[pos]).to eq(grid[0][0])
-      end
+    it 'should not set the grid as an empty array' do
+      expect(grid).not_to eq([])
+    end
+  end
+
+  describe '#[]' do
+    it 'should accept a pair of indices to represent the chosen position on the board' do
+      pos = [0, 0]
+      expect(board[pos]).to eq(grid[0][0])
+    end
+  end
+
+  describe '#[]=' do
+    it 'should set the position on the grid to the given symbol' do
+      pos = [1, 1]
+      grid[1][1] = 'X'
+      expect(board[pos]).to eq('X')
     end
 
-    describe '#[]=' do
-      it 'should set the position on the grid to the given symbol' do
-        pos =[1, 1]
-        grid[1][1] = 'X'
-        expect(board[pos]).to eq('X')
-      end
+    it 'should not set the position on the grid to an incorrect symbol' do
+      pos = [1, 1]
+      grid[1][1] = 'X'
+      expect(board[pos]).not_to eq('O')
+    end
+  end
 
-      it 'should not set the position on the grid to an incorrect symbol' do
-        pos =[1, 1]
-        grid[1][1] = 'X'
-        expect(board[pos]).not_to eq('O')
-      end
+  describe '#valid?' do
+    it 'should check if the given position exists on the board' do
+      pos = [2, 2]
+      expect(board.valid?(pos)).to eq(true)
     end
 
-    describe '#valid?' do
-      it 'should check if the given position exists on the board' do
-        pos = [2, 2]
-        expect(board.valid?(pos)).to eq(true)
-      end
-
-      it 'should return false if the given position does not exist on the board' do
-        pos = [3, 3]
-        expect(board.valid?(pos)).to eq(false)
-      end
+    it 'should return false if the given position does not exist on the board' do
+      pos = [3, 3]
+      expect(board.valid?(pos)).to eq(false)
     end
-
+  end
 end
