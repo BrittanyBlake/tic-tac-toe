dda5058884dde0b41506f4db92db4b2ac72627c5
Brittany Blake <brittany.blake49@gmail.com>
2020-05-12 10:37:24 +0200

    Add spec for board#[]=

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -14,11 +14,25 @@ RSpec.describe Board do
         expect(grid).not_to eq([])
       end
     end
-    
+
     describe '#[]' do
       it 'should accept a pair of indices to represent the chosen position on the board' do
          pos = [0, 0]
         expect(board[pos]).to eq(grid[0][0])
+      end
+    end
+
+    describe '#[]=' do
+      it 'should set the position on the grid to the given symbol' do
+        pos =[1, 1]
+        grid[1][1] = 'X'
+        expect(board[pos]).to eq('X')
+      end
+
+      it 'should not set the position on the grid to an incorrect symbol' do
+        pos =[1, 1]
+        grid[1][1] = 'X'
+        expect(board[pos]).not_to eq('O')
       end
     end
 
